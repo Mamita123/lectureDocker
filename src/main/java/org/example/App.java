808ca58acc490d60f26a5750101e99fd808b9d2e
Mamita <mamitagurung895@gmail.com>
2024-03-01 13:51:45 +0200

    modified commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,11 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-}
+        System.out.println( "Hello World!" + add(1,2)); }}
